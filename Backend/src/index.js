7b6e8138fcb8c30b6660b024302e130a95a3160f
Vishal-Agarwal77 <7793839858vishal@gmail.com>
2024-05-12 18:00:04 +0530

    Port modified

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,11 +2,16 @@ import dotenv from 'dotenv'
 import { connectToDb } from './db/index.js'
 import { app } from './app.js'
 
+dotenv.config({
+    path: './.env'
+})
+
+const port=process.env.PORT || 4000
 
 connectToDb()
     .then(() => {
         
-        app.listen(3000, () => {
-            console.log(`Server is live at ${process.env.PORT}`)
+        app.listen(port, () => {
+            console.log(`Server is live at ${port}`)
         })
     })
