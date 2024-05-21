047d48fe18244db1e619e1e039d5a6ccaac2ca44
Vishal-Agarwal77 <7793839858vishal@gmail.com>
2024-05-21 15:40:34 +0530

    Cors origin changed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ app.use(cookieParser())
 
 app.use(cors({
     credentials: true,
-    origin: ["https://fitnesspro-1.onrender.com",'http://localhost:5173']
+    origin: "https://fitnesspro-1.onrender.com"
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
