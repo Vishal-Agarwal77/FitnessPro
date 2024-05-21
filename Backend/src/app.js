fb7fc84fa3f7d9cc5db06a5272cbff23729d0974
Vishal-Agarwal77 <7793839858vishal@gmail.com>
2024-05-21 15:53:25 +0530

    minor Changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,12 +5,12 @@ import router from "./routes/user.routes.js";
 
 const app = express();
 
-app.use(cookieParser())
-
 app.use(cors({
     credentials: true,
     origin: "https://fitnesspro-1.onrender.com"
 }))
+
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
